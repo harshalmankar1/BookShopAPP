631fd95f353e5d11dd6c68420f451a05ce54f395
Harshal Mankar <hashalmankar1099@gmail.com>
2024-08-30 12:26:49 +0530

    changes

--- src/app/addbook/addbook.component.ts
+++ src/app/addbook/addbook.component.ts
@@ -17,6 +17,7 @@ export class AddbookComponent {
 
   }
   add(){
+    debugger
     console.log("data added successfully",this.book)
     this.bookservice.addbook(this.book).subscribe(data=>{console.log("data posted successfully") ;alert("data added successfully"+data);this.router.navigate(['/home'])},error=>{console.log("error")})
     }
